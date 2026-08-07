259ba795ce0ba068409107bc39ec5b1df365056e
Yhara Landi <landiyhara@gmail.com>
2026-08-07 14:55:32 +0200

    feat:
    Ho visto che mi andava in errore di compilazione, a quanto pare mancava l'annotazione @repository che specificava che quell'interfaccia era gestita da spring. Ho visto che in ogni caso compilava e funzionava ma era fastidioso l'errore ed ho trovato questa soluzione.
    
    Praticamente con essa stiamo dicendo a spring "questo è un repository gestito dal container"

--- src/main/java/com/SocialNetwork/SocialNetwork/repository/LikeRepository.java
+++ src/main/java/com/SocialNetwork/SocialNetwork/repository/LikeRepository.java
@@ -3,9 +3,11 @@ package com.SocialNetwork.SocialNetwork.repository;
 import com.SocialNetwork.SocialNetwork.model.Like;
 import com.SocialNetwork.SocialNetwork.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
     List<Like> findByPost(Post post);
 }
--- src/main/java/com/SocialNetwork/SocialNetwork/repository/UtenteRepository.java
+++ src/main/java/com/SocialNetwork/SocialNetwork/repository/UtenteRepository.java
@@ -2,9 +2,11 @@ package com.SocialNetwork.SocialNetwork.repository;
 
 import com.SocialNetwork.SocialNetwork.model.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UtenteRepository extends JpaRepository<Utente, Long> {
     Optional<Utente> findByUsername(String username);
 }
